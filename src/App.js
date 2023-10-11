3ca119045e5a2e2b57f7f53d0af2514485e3452a
tuanlvph22106 <tuanlvph22106@fpt.edu.vn>
2023-10-11 11:35:04 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Xin Chao Anh Tuan Dep Trai
         </p>
         <a
           className="App-link"
