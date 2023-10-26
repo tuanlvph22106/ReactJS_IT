bf10ea457c3e2cdf9b0583982796392c35f6119b
tuanlvph22106 <tuanlvph22106@fpt.edu.vn>
2023-10-26 10:10:04 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Xin Chao Anh Tuan Dep Trai
+          Xin Chao Tuấn Vân
         </p>
         <a
           className="App-link"
